cff893dda4b43d0813b0845b8d49ff00e8e4eb29
Matthew Feng <netsec.phs@gmail.com>
2015-01-30 09:50:42 -0500

    stupid

--- portread.py
+++ portread.py
@@ -3,7 +3,7 @@ import time;
 import os;
 
 #data = serial.Serial('/dev/ttys008', 9600);
-data = serial.Serial('/dev/ttyACM0', 9600);
+data = serial.Serial('/dev/ttyACM1', 9600);
 
 while True:
     indata = data.readline().strip();
